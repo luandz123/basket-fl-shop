127bba7f5f481e9b42aba37b689133a49b407053
luandz123 <LuanNV.B23CC106@stu.ptit.edu.vn>
2025-03-28 20:32:51 +0700

    them main

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,7 +4,9 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import  helmet from 'helmet';
 import * as compression from 'compression';
-
+if (!(global as any).crypto) {
+  (global as any).crypto = crypto;
+}
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
