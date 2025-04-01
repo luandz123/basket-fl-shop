e84046b1a775232f9fc0cac7c217e27f4718e4da
luandz123 <LuanNV.B23CC106@stu.ptit.edu.vn>
2025-04-01 19:43:53 +0700

    prod

---
+++ backend/webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+const webpack = require('webpack');
+const nodeExternals = require('webpack-node-externals');
+
+module.exports = {
+  entry: ['./src/main.ts'],
+  target: 'node',
+  externals: [nodeExternals()],
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'main.js',
+  },
+};
